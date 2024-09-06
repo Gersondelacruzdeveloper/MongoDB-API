b1a5410e7c2f9b13de25cf63a3b90ba121ae9a32
Gerson <gdelacruz.developer  @gmail.com>
2024-09-06 10:42:21 -0400

    Add SIMPLE_JWT

--- backend/threats/views.py
+++ backend/threats/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # threats/views.py
 from rest_framework import generics
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
