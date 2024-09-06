14d0d73609757b27786d2ec717be4c0ec6757966
Gerson <gdelacruz.developer  @gmail.com>
2024-09-06 09:21:54 -0400

    Add registration

--- backend/threats/urls.py
+++ backend/threats/urls.py
@@ -1,8 +1,9 @@
 # threats/urls.py
 from django.urls import path
-from .views import ThreatListCreateView, ThreatDetailView, ApiOverview
+from .views import ThreatListCreateView, ThreatDetailView, ApiOverview, register_user
 
 urlpatterns = [
+    path('register/', register_user, name='register_user'),
     path('threats/', ThreatListCreateView.as_view(), name='threat-list-create'),
     path('threats/<str:pk>/', ThreatDetailView.as_view(), name='threat-detail'),
     path('overview/', ApiOverview.as_view(), name='api-overview'),
--- backend/threats/views.py
+++ backend/threats/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 # threats/views.py
 from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from .models import CyberThreat
 from rest_framework.views import APIView
 from .serializers import CyberThreatSerializer
+from rest_framework.decorators import api_view, permission_classes
 
 class ApiOverview(APIView):
     permission_classes = [AllowAny]
@@ -33,3 +36,21 @@ class ThreatDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = CyberThreat.objects.all()
     serializer_class = CyberThreatSerializer
     permission_classes = [IsAuthenticated]
+
+
+#------------------------------------------------ django fuctions
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    email = request.data.get('email')
+
+    if username is None or password is None:
+        return Response({'error': 'Please provide both username and password'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if User.objects.filter(username=username).exists():
+        return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create_user(username=username, password=password, email=email)
+    return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
