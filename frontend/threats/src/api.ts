03563b6bd60dc6bb5b8fe6bc98b78da326843540
Gerson <gdelacruz.developer  @gmail.com>
2024-09-06 07:54:17 -0400

    Add TypeScript interface for cyberthreats and managing API url

---
+++ frontend/threats/src/api.ts
@@ -0,0 +1,17 @@
+// src/api.ts
+import axios from 'axios';
+import { CyberThreat } from './types';
+
+const API_URL = 'http://localhost:8000/api/v1/threats/';
+
+// Fetch all cyberthreats
+export const getCyberThreats = async (): Promise<CyberThreat[]> => {
+    const response = await axios.get(API_URL);
+    return response.data;
+};
+
+// Add a new cyberthreat
+export const addCyberThreat = async (newThreat: CyberThreat): Promise<CyberThreat> => {
+    const response = await axios.post(API_URL, newThreat);
+    return response.data;
+};
---
+++ frontend/threats/src/types.ts
@@ -0,0 +1,7 @@
+// src/types.ts
+export interface CyberThreat {
+    threat_id: string;
+    description: string;
+    severity_level: string;
+    source: string;
+}
